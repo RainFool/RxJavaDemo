3ea8c4fd7b7abf84021506f04d5c30dec363f15c
RainFool <rainfool1024@gmail.com>
2018-01-23 21:16:24 +0800

    create操作符初探

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -1,10 +1,12 @@
 package src;
 
 import io.reactivex.Observable;
+import src.create.CreateSample;
 
 public class Main {
     public static final void main(String[] args) {
-        System.out.println("main");
-
+        System.out.println("--------main----------");
+        CreateSample createSample = new CreateSample();
+        createSample.excute();
     }
 }
--- src/main/java/src/create/CreateSample.java
+++ src/main/java/src/create/CreateSample.java
@@ -1,15 +1,28 @@
 package src.create;
 
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Consumer;
 import src.ISample;
+
+import java.util.concurrent.Callable;
 
 public class CreateSample implements ISample {
 
     public CreateSample() {
+
     }
 
     @Override
     public void excute(String... params) {
+        sampleCreate();
+    }
 
+    private void sampleCreate() {
+        Flowable.just("Hello world").subscribe(System.out::println);
+        Flowable.fromCallable(() -> "from callable")
+                .subscribe(System.out::println);
     }
 }
